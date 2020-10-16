e2b3e7c88f5f723c18312b4556257979e2ee9066
detalikota <detalikota@gmail.com>
2020-10-16 09:07:23 +0300

    new files

--- head.py
+++ head.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from vsearch import search4letter
+from vsearch import search4letters
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,8 +7,8 @@ def hello() -> str:
     return 'Hello from Flask!'
 
 @app.route('/search4')
-def do_search(x,y):
-    return search4letter('life, the universe, and everything!', 'eiru,!')
+def do_search():
+    return str(search4letters ('life, the universe, and everything!', 'eiru,!'))
 
 
 app.run()
