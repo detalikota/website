557bd693332ecf0d7244bdb4f25d00f6b1298181
detalikota <detalikota@gmail.com>
2020-10-17 15:30:04 +0300

    added files

--- modules/vsearch.py
+++ modules/vsearch.py
@@ -2,4 +2,8 @@ def search4vowels(phrase: str) -> set:
      return set('aeiou').intersection(set(phrase))
 
 def search4letters(word,letters):
-    return set(word).intersection(set(letters))
+    x= set(word).intersection(set(letters))
+    if x == set():
+         return str("nothing")
+    else:
+         return x
--- vsearch4web.py
+++ vsearch4web.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello from Flask!'
-
 @app.route('/search4', methods=['POST'])
 def do_search():
-    return str(search4letters ('life, the universe, and everything!', 'eiru,!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))    
+    return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', 
     the_title='Welcome to search4letters on the web!')
-@app.route('/1')
-def test():
-    return render_template('entry.html')
 
-app.run()
+if __name__=='__main__':
+    app.run(debug=True)
