56d4924e772498dea1d036719f96ae9b908d063e
detalikota <detalikota@gmail.com>
2020-10-21 11:25:03 +0300

    new changes

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 app = Flask(__name__)
 
 def log_request (req: 'flask_request', res: str) -> None:
     with open('log.txt', 'a') as log:
-        print (req,res, file=log)
+        print (req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search4', methods=['POST'])
 def do_search():
@@ -21,8 +21,12 @@ def entry_page() -> 'html':
     the_title='Welcome to search4letters on the web!')
 @app.route('/viewlog')
 def view_the_log() -> str:
+    contents = []
     with open('log.txt') as log:
-        contents = log.read()
-    return contents
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    return str(contents)
 if __name__=='__main__':
     app.run(debug=True)
