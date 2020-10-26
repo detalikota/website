76eec1ab3d75c8d9e192334cb13c427fac13d7aa
detalikota <detalikota@gmail.com>
2020-10-26 11:28:34 +0300

    hei

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+import mysql.connector
 app = Flask(__name__)
 
 def log_request (req: 'flask_request', res: str) -> None:
-    with open('log.txt', 'a') as log:
-        print (req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    dbconfig = { 'host': '127.0.0.1', 'user': 'root', 'password': 'ProgEllco1!', 'database': 'vsearchlogDB',}
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form ['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 @app.route('/search4', methods=['POST'])
 def do_search():
@@ -20,17 +27,14 @@ def entry_page() -> 'html':
     return render_template('entry.html', 
     the_title='Welcome to search4letters on the web!')
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
     contents = []
     with open('log.txt') as log:
         for line in log:
             contents.append([])
             for item in line.split('|'):
                 contents[-1].append(escape(item))
-    return str(contents)
-<<<<<<< HEAD
-    return contents
-=======
->>>>>>> 56d4924e772498dea1d036719f96ae9b908d063e
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents,)
 if __name__=='__main__':
     app.run(debug=True)
