7eb75c52697ed7fbe77a2b5178117049bdd1bdd6
detalikota <detalikota@gmail.com>
2020-11-02 22:20:04 +0300

    some change

--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
-from flask import escape
-escape('This is a <Request>')
-print (escape())
+from DBcm import UseDatabase
+
+dbconfig = { 'host': '127.0.0.1', 'user': 'root', 'password': 'ProgEllco1!', 'database': 'vsearchlogDB',}
+
+with UseDatabase(dbconfig) as cursor:
+    _SQL = """show tables"""
+    cursor.execute(_SQL)
+    data = cursor.fetchall()
+print (data)
---
+++ tests2.py
@@ -0,0 +1,8 @@
+class Magomed:
+    def __init__(self, a=None, b=None):
+        self.a = 5
+        self.b = 10
+        c = a + b
+        return c
+hi = Magomed()
+print (hi())
